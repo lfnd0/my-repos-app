307b0913e3c7fb4f2297f95ffd86d5965b6cbac2
luizfernando <luiz-fsilva37@hotmail.com>
2024-01-24 20:00:29 -0300

    Carregando os detalhes

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,9 +1,38 @@
 import { useParams } from "react-router-dom";
+import { Container } from "./styles";
+import { useEffect, useState } from "react";
+import API from "../../api/services";
 
 export default function Repository() {
   const { name } = useParams();
 
+  const [repository, setRepository] = useState({});
+  const [issues, setIssues] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function getRepository() {
+      const [repositoryData, issuesData] = await Promise.all([
+        await API.get(`repos/${name}`),
+        await API.get(`repos/${name}/issues`, {
+          params: {
+            state: 'open',
+            per_page: 5,
+          }
+        }),
+      ]);
+
+      setRepository(repositoryData.data);
+      setIssues(issuesData.data);
+      setLoading(false);
+    }
+
+    getRepository();
+  }, [name]);
+
   return (
-    <h1>{name}</h1>
+    <Container>
+      {name}
+    </Container>
   );
 }
